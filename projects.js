c85ad1e5fc6e1b742125c005cca8d4c5ff20a36b
Calum Wilson <c.wilson@strath.ac.uk>
2025-08-19 12:32:12 +0100

    Create projects.js

---
+++ projects.js
@@ -0,0 +1,98 @@
+const researchProjects = [
+  {
+    id: 1,
+    title: "Investigating the cardiotoxic effects of cobalt on human cardiac fibroblasts",
+    type: "Experimental Research Projects (Laboratory)",
+    students: 2,
+    chosenBy: 65,
+    subjects: ["Biochemistry", "Biomedical Science", "Pharmacology"],
+    outline: "Arthritis of the hip continues to be a leading cause of chronic pain for patients across the UK and current therapy for advanced cases comprises hip replacement. Although technology has improved over the years in terms of the design of hip prostheses, there continue to be major issues associated with the health and overall outlook for patients receiving artificial hips. There is now very strong evidence supporting a link between metal-on-metal (MoM) hip replacements (where metals such as cobalt and chromium are used in the artificial joint bearings) and development of heart disease. Over time, cobalt levels rise in the bloodstream of patients with MoM hip replacements and the metal accumulates in various organs of the body. Levels accumulating in the heart can be particularly high. Previous work by our group using rats has shown that cobalt can cause impaired contractile activity of rat hearts. The cellular mechanisms underlying this cobalt-mediated contractile dysfunction may manifest at the level of both contractile and non-contractile cells of the heart. Our work to date has focused on a rat model and we have not explored the effects of cobalt in human cardiac cells. Cardiac fibroblasts are non-contractile cells responsible for providing structural support to the heart via the extracellular matrix and they interact closely with the contractile cardiac myocytes.",
+    aim: "This project will investigate whether cobalt may exert detrimental effects on human cardiac fibroblasts and will explore both phenotypic and functional changes at the cellular level.",
+    techniques: ["Cell culture", "Pharmacological treatment of cells", "Protein assays", "immunoblotting", "cell imaging"],
+    primarySupervisor: "Dr. Sarah Currie",
+    secondarySupervisor: "",
+    references: [
+      "Jenkinson M., Meek R.D., Tate R., MacMillan S., Grant M.H. & Currie S. (2021) Cobalt-induced cardiomyopathy – do circulating cobalt levels matter? Bone Joint Res 10(6):340-347",
+      "Jenkinson M.J., Meek R.M.D., Tate R.J., Brady A., MacMillan S., Grant M.H. & Currie S. (2024) Cardiac function may be compromised in patients with elevated blood cobalt levels secondary to metal-on-metal hip implants. Bone Joint J. 106B, 51-58",
+      "Porter K.E. & Turner N.A. (2009) Cardiac fibroblasts: At the heart of myocardial remodelling. Pharm. Ther. 123, 255-278",
+      "Harrington A. & Moore-Morris T. (2024) Cardiac fibroblasts in heart failure and regeneration. Front Cell Dev Biol doi.org/10.3389/fcell.2024.1388378"
+    ]
+  },
+  {
+    id: 2,
+    title: "Repurposing combinations of drugs for cancer therapy",
+    type: "Experimental Research Projects (Laboratory)",
+    students: 3,
+    chosenBy: 75,
+    subjects: ["Biochemistry", "Biomedical Science", "Pharmacology"],
+    outline: "While we are doing well in the treatment of some cancers, there are still many cancers where survival rates are poor. This is in part due to the heterogeneity of cancers and underlying therapy resistance. Despite the considerable resources being invested into the discovery of new cancer drugs, novel therapies suffer from high failure rates, high cost, limited efficacy and a lengthy timeline before they become clinically available. None of this is good for the cancer patient. One approach to circumvent this problem is to repurpose existing non-cancer or cancer drugs against cancers to provide new or novel therapies or to consider new mays of formulating and delivering existing drugs, which could reduce costs, enhance tumour specificity, reduce toxicity or rapidly accelerate the introduction of new therapies into clinical practice. This is particularly relevant for paediatric and hard to treat cancers such as pancreatic cancer, brain tumours, lung cancer and triple negative breast cancer and cancers where there is currently an unmet need for effective less toxic cancer drugs.",
+    aim: "The aim of this Project will be to undertake a variety of experiments to assess new therapies for cancer",
+    techniques: ["Cell culture", "drug formulation and use of nanomaterials", "DNA damage and repair assays", "assessment of cell death", "cell survival assays"],
+    primarySupervisor: "Dr. Helen Cooke",
+    secondarySupervisor: "",
+    references: [
+      "Jin, H., Wang, L. & Bernards, R. Rational combinations of targeted cancer therapies: background, advances and challenges. Nat Rev Drug Discov 22, 213–234 (2023).",
+      "Dattilo, M.; Patitucci, F.; Prete, S.; Parisi, O.I.; Puoci, F. Polysaccharide-Based Hydrogels and Their Application as Drug Delivery Systems in Cancer Treatment: A Review. J. Funct. Biomater. 2023, 14, 55."
+    ]
+  },
+  {
+    id: 3,
+    title: "Repurposing combinations of drugs for cancer therapy",
+    type: "Data Science: Data Analysis",
+    students: 3,
+    chosenBy: 68,
+    subjects: ["Biochemistry", "Biomedical Science", "Pharmacology"],
+    outline: "Despite the considerable resources being invested into the discovery of new cancer drugs, novel therapies suffer from high failure rates, high cost, limited efficacy and a lengthy timeline before they become clinically available. None of this is good for the cancer patient. One approach to circumvent this problem is to repurpose existing non-cancer or cancer drugs against cancers to provide new or novel therapies or new formulation of existing therapies which could reduce costs, enhance tumour specificity, reduce toxicity or rapidly accelerate the introduction of new therapies into clinical practice. This is particularly relevant for paediatric and hard to treat cancers such as pancreatic cancer, brain tumours, lung cancer, ovarian cancer and triple negative breast cancer and cancers where there is currently an unmet need for effective less toxic cancer drugs. Furthermore often the primary cancer is not the therapeutic issue, but the metastatic and therapy resistant cancer that returns. New therapies are therefore needed for the treatment of hard to treat and therapy resistant cancer.",
+    aim: "The aim of this project is to observe laboratory techniques assessing the efficacy of combination therapies and then to analyse real data derived from a range of experimental techniques to determine the efficacy and mode of action of combination therapies.",
+    techniques: ["data analysis", "Laboratory observation", "Critical appraisal", "scientific writing", "literature search"],
+    primarySupervisor: "Dr. Helen Cooke",
+    secondarySupervisor: "",
+    references: [
+      "Jin, H., Wang, L. & Bernards, R. Rational combinations of targeted cancer therapies: background, advances and challenges. Nat Rev Drug Discov 22, 213–234 (2023).",
+      "Subash C. Gupta1, Bokyung Sung1, Sahdeo Prasad1, Lauren J. Webb2, and Bharat B. Aggarwal1 Cancer drug discovery by repurposing: teaching new tricks to old dogs. Trends in Pharmacological Sciences September 2013, Vol. 34, No. 9",
+      "Bayat Mokhtari R, Homayouni TS, Baluch N, Morgatskaya E, Kumar S, Das B, Yeger H. Combination therapy in combating cancer. Oncotarget. 2017 Jun 6;8(23):38022-38043."
+    ]
+  },
+  {
+    id: 4,
+    title: "All the world's a phage: the diversity of bacteriophage that infect Streptomyces",
+    type: "Data Science: Bioinformatics",
+    students: 4,
+    chosenBy: 26,
+    subjects: ["Biomedical Science", "Microbiology"],
+    outline: "Bacteriophage are ubiquitous and thought to be the most abundant organisms on the planet, and they play key roles in bacterial horizontal gene transfer, ecology, and evolution. However, there is relatively little known about the bacteriophage that infect Streptomyces, in spite of the fact that bacteria from this genus are of great ecological importance, given their roles as saprophytes and in plant and insect interactions, and of great biotechnological importance due to their ability to produce useful natural products",
+    aim: "to analyse the diversity of bacteriophages that infect Streptomyces species",
+    techniques: ["literature review", "bioinformatics", "data analysis"],
+    primarySupervisor: "Dr. Paul Hoskisson",
+    secondarySupervisor: "",
+    references: [
+      "Mahmoudabadi, G. & Phillips, R., 2018. A comprehensive and quantitative exploration of thousands of viral genomes. eLife, 7, p.e31955.",
+      "Edwards, R.A., et al., 2016. Computational approaches to predict bacteriophage-host relationships. FEMS Microbiology Reviews, 40(2), pp.258-272.",
+      "Jaffal, H., et al., 2024. Prophage induction can facilitate the in vitro dispersal of multicellular Streptomyces structures. PLOS Biology, 22(7), p.e3002725.",
+      "Kronheim, S., Solomon, E., Ho, L., Glossop, M., Davidson, A.R. & Maxwell, K.L., 2023. Complete genomes and comparative analyses of Streptomyces phages that influence secondary metabolism and sporulation. Scientific Reports, 13(1), p.9820.",
+      "Luthe, T., et al., 2023. Streptomyces development is involved in the efficient containment of viral infections. microLife, 4, p.uqad002.",
+      "Choufa, C., et al., 2022. Prevalence and mobility of integrative and conjugative elements within a Streptomyces natural population. Frontiers in Microbiology, 13, p.970179.",
+      "Heller, D.M., et al., 2024. SEA-PHAGES and SEA-GENES: Advancing virology and science education. Annual Review of Virology"
+    ]
+  },
+  {
+    id: 5,
+    title: "Analysis of genetic changes in cancer",
+    type: "Data Science: Data Analysis",
+    students: 1,
+    chosenBy: 71,
+    subjects: ["Biochemistry", "Biomedical Science", "Immunology", "Pharmacology"],
+    outline: "A wealth of information about genomic/genetic changes that occur in different cancers and how this affects patient outcomes is contained in public databases such as The Cancer Genome Atlas (TCGA). The aim of this data analysis project is to extract and analyse data on two genes of interest from public databases to address the following questions: (i) are the genes of interest known tumour drivers or suppressors? (ii) How does the mRNA expression profile differ between healthy and tumour tissue? (iii) Does the level of gene expression affect patient outcome? (iv) What are the variant profiles of your genes of interest in different cancers? (v) Do your genes/protein of interest interact with any known tumour drivers or suppressors?",
+    aim: "To explore the association between two genes of interest and cancer. Students are able to choose which genes they would like to study or follow suggestions made by the supervisor.",
+    techniques: ["Use of public databases", "Data analysis", "presentation", "critical review"],
+    primarySupervisor: "Dr. Joanna Young",
+    secondarySupervisor: "",
+    references: [
+      "Ko, P.J., and Dixon, S.J. (2018). Protein palmitoylation and cancer. EMBO reports 19(10):e46666",
+      "https://www.cancer.gov/ccg/research/genome-sequencing/tcga"
+    ]
+  }
+];
+
+// Export for use in other modules
+export default researchProjects;
